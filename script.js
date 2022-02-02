7c07fe0ac660d8dbb9e5ae6202aa3237c853c7c4
JReilly55 <98059081+JReilly55@users.noreply.github.com>
2022-02-02 15:55:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,18 +5,16 @@ var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
 
+
+// Crime JSON
 $.getJSON("https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%201%20-%20GitHub%20and%20Leaflet/sf_crime.geojson",function(data){
     var Icon = L.icon({
     iconUrl: 'https://icons.iconarchive.com/icons/3xhumed/mega-games-pack-22/256/The-Godfather-1-icon.png',
     iconSize: [50,50]
   }); 
- var crime = L.geoJson(data,{
-      pointToLayer: function(feature,latlng){
-        var marker = L.marker(latlng,{icon: Icon});
-        return marker;
-      }
-    });
-    var clusters = L.markerClusterGroup();
-    clusters.addLayer(crime);
-    mymap.addLayer(clusters);
+  L.geoJson(data,{
+    pointToLayer: function(feature,latlng){
+	  return L.marker(latlng,{icon: Icon});
+    }
+  }).addTo(mymap);
 });
